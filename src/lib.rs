538e224dd830409c2e91bbd3b95618d767cf63f1
jakubbartolomej <jakubkosuth@gmail.com>
2019-10-27 14:37:46 +0100

    Make call of decode_hex safe(check input before parsing try)

--- src/lib.rs
+++ src/lib.rs
@@ -6,11 +6,27 @@ use wordlist::WORD_LIST;
 use std::{num::ParseIntError};
 
 
+/// Checks whether given string is hexadecimal
+pub fn is_hexadecimal(text: &str) -> bool {
+    if text.len() % 2 == 1 {
+        return false;
+    }
+    for character in text.chars() {
+        if !character.is_ascii_hexdigit() {
+            return false;
+        }
+    }
+    true
+}
+
 // https://stackoverflow.com/questions/52987181/how-can-i-convert-a-hex-string-to-a-u8-slice
-pub fn decode_hex(s: &str) -> Result<Vec<u8>, ParseIntError> {
-    (0..s.len())
+pub fn decode_hex(input: &str) -> Result<Vec<u8>, ParseIntError> {
+    if !is_hexadecimal(input) {
+        panic!("Invalid input");
+    }
+    (0..input.len())
         .step_by(2)
-        .map(|i| u8::from_str_radix(&s[i..i + 2], 16))
+        .map(|i| u8::from_str_radix(&input[i..i + 2], 16))
         .collect()
 }
 
